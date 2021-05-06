ab9520af197772cb67af5f856204b9609a398bc6
Artsiom Krukouski <ArtiomKrukovsky@gmail.com>
2021-05-07 01:10:55 +0300

    git init

---
+++ ServiceBus.Producer/Controllers/MessagingController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceBus.Producer.Models;
+using ServiceBus.Producer.Requests;
+using ServiceBus.Producer.Services;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ServiceBus.Producer.Controllers
+{
+    public class MessagingController : ControllerBase
+    {
+        private readonly IMessagePublisher _messagePublisher;
+
+        public MessagingController(IMessagePublisher messagePublisher)
+        {
+            _messagePublisher = messagePublisher;
+        }
+
+        [HttpPost]
+        [Route("publish/text")]
+        public async Task<IActionResult> PublishText()
+        {
+            using (var reader = new StreamReader(Request.Body))
+            {
+                var bodyAsText = await reader.ReadToEndAsync();
+                await _messagePublisher.Publish(bodyAsText);
+            };
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("publish/order")]
+        public async Task<IActionResult> PublishOrder([FromBody] CreateOrderRequest request)
+        {
+            var order = new Order
+            {
+                Id = request.Id,
+                ProductName = request.ProductName
+            };
+
+            await _messagePublisher.Publish(order);
+            return Ok();
+        }
+    }
+}
---
+++ ServiceBus.Producer/Services/MessagePublisher.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBus.Producer.Services
+{
+    public class MessagePublisher : IMessagePublisher
+    {
+        public readonly ITopicClient _topicClient;
+
+        public MessagePublisher(ITopicClient topicClient)
+        {
+            _topicClient = topicClient;
+        }
+
+        public async Task Publish<T>(T obj)
+        {
+            // convert the object to transformable type
+            var objAsText = JsonConvert.SerializeObject(obj);
+            var message = new Message(Encoding.UTF8.GetBytes(objAsText));
+
+            // add property value uses by filter
+            message.UserProperties[Constants.Constants.MessageType] = typeof(T).Name;
+
+            // send the message to the topic
+            await _topicClient.SendAsync(message);
+        }
+
+        public async Task Publish(string raw)
+        {
+            var message = new Message(Encoding.UTF8.GetBytes(raw));
+
+            message.UserProperties[Constants.Constants.MessageType] = "raw";
+
+            await _topicClient.SendAsync(message);
+        }
+    }
+}
